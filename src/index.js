cc2c0d2f27aad220a9737a8453a23e1c458fe2e8
Theresa Tran <theresa1194@gmail.com>
2023-10-23 22:45:58 +1100

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form');
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+        toDoListText(e.target.querySelector('#new-task-description').value);
+        form.reset();
+    });
 });
+
+
+function toDoListText(toDo) {
+  let p = document.createElement('p');
+  let btn = document.createElement('button');
+  btn.addEventListener('click', deleteBtn);
+  btn.textContent = 'x';
+  p.textContent = `${toDo}  `;
+  p.appendChild(btn);
+  
+
+// editing the text:
+p.addEventListener('dblclick', function() {
+  p.contentEditable = true;
+  p.focus();
+});
+
+p.addEventListener('blur', function() {
+  p.contentEditable = false;
+  
+})
+
+document.querySelector('#tasks').appendChild(p);
+}
+
+
+function deleteBtn(e) {
+  e.target.parentNode.remove();
+}
+
